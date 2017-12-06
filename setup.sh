a67aa263cd16bdbd09baffb1d49baf0b53557dbb
vivekvelso <34197929+vivekvelso@users.noreply.github.com>
2017-12-05 21:34:54 -0500

    fix

--- setup.sh
+++ setup.sh
@@ -6,4 +6,9 @@ sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install nodejs
 
-npm install
+npm install node-sass request@2.81.0 shrinkwrap
+npm init # make sure <=2.81.0
+npm prune
+npm shrinkwrap
+
+
